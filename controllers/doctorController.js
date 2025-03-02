3760e760a7d5fc3d593fb36f299915d37e5673e9
FazleRahmanEvan <evanshakib26@gmail.com>
2025-03-02 23:43:09 +0600

    register

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,5 +1,16 @@
 import doctorModel from "../models/doctorModel.js";
 
+// API to get all doctors list for Frontend
+const doctorList = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({}).select(["-password", "-email"]);
+    res.json({ success: true, doctors });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
 // API to change doctor availablity for Admin and Doctor Panel
 const changeAvailablity = async (req, res) => {
   try {
@@ -16,4 +27,4 @@ const changeAvailablity = async (req, res) => {
   }
 };
 
-export { changeAvailablity };
+export { changeAvailablity, doctorList };
