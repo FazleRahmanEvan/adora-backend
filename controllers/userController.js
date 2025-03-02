786c1c5023321e00eb9301be1294667f1cad5969
FazleRahmanEvan <evanshakib26@gmail.com>
2025-03-03 00:36:54 +0600

    user register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
-import userModel from "../models/userModel";
+import userModel from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import validator from "validator";
 
 // API to register user
 const registerUser = async (req, res) => {
@@ -49,4 +50,41 @@ const registerUser = async (req, res) => {
   }
 };
 
-export { registerUser };
+// API to login user
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "User does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to get user profile data
+const getProfile = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const userData = await userModel.findById(userId).select("-password");
+
+    res.json({ success: true, userData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { registerUser, loginUser, getProfile };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { registerUser } from "../controllers/userController";
+import {
+  getProfile,
+  loginUser,
+  registerUser,
+} from "../controllers/userController.js";
+import authUser from "../middlewares/authUser.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/get-profile", authUser, getProfile);
 
 export default userRouter;
