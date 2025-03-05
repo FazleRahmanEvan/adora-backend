295ba1163451bad81070aad3078905d93f9649b7
FazleRahmanEvan <evanshakib26@gmail.com>
2025-03-06 04:13:57 +0600

    doclogin

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,4 +1,30 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import doctorModel from "../models/doctorModel.js";
+
+// API for doctor Login
+const loginDoctor = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await doctorModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // API to get all doctors list for Frontend
 const doctorList = async (req, res) => {
@@ -27,4 +53,4 @@ const changeAvailablity = async (req, res) => {
   }
 };
 
-export { changeAvailablity, doctorList };
+export { changeAvailablity, doctorList, loginDoctor };
