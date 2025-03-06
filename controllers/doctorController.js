c39ff19ea4f9ffc22c8d9424678572fc4b2e3e2a
FazleRahmanEvan <evanshakib26@gmail.com>
2025-03-06 21:02:19 +0600

    dashdoctor

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import doctorModel from "../models/doctorModel.js";
+import appointmentModel from "../models/appointmentModel.js";
 
 // API for doctor Login
 const loginDoctor = async (req, res) => {
@@ -53,4 +54,139 @@ const changeAvailablity = async (req, res) => {
   }
 };
 
-export { changeAvailablity, doctorList, loginDoctor };
+// API to get doctor appointments for doctor panel
+const appointmentsDoctor = async (req, res) => {
+  try {
+    const { docId } = req.body;
+    const appointments = await appointmentModel.find({ docId });
+
+    res.json({ success: true, appointments });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to cancel appointment for doctor panel
+const appointmentCancel = async (req, res) => {
+  try {
+    const { docId, appointmentId } = req.body;
+
+    const appointmentData = await appointmentModel.findById(appointmentId);
+    if (appointmentData && appointmentData.docId === docId) {
+      await appointmentModel.findByIdAndUpdate(appointmentId, {
+        cancelled: true,
+      });
+      return res.json({ success: true, message: "Appointment Cancelled" });
+    }
+
+    res.json({ success: false, message: "Appointment Cancelled" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to mark appointment completed for doctor panel
+const appointmentComplete = async (req, res) => {
+  try {
+    const { docId, appointmentId } = req.body;
+
+    const appointmentData = await appointmentModel.findById(appointmentId);
+    if (appointmentData && appointmentData.docId === docId) {
+      await appointmentModel.findByIdAndUpdate(appointmentId, {
+        isCompleted: true,
+      });
+      return res.json({ success: true, message: "Appointment Completed" });
+    }
+
+    res.json({ success: false, message: "Appointment Cancelled" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to get dashboard data for doctor panel
+const doctorDashboard = async (req, res) => {
+  try {
+    const { docId } = req.body;
+
+    const appointments = await appointmentModel.find({ docId });
+
+    let earnings = 0;
+
+    appointments.map((item) => {
+      if (item.isCompleted || item.payment) {
+        earnings += item.amount;
+      }
+    });
+
+    let patients = [];
+
+    appointments.map((item) => {
+      if (!patients.includes(item.userId)) {
+        patients.push(item.userId);
+      }
+    });
+
+    const dashData = {
+      earnings,
+      appointments: appointments.length,
+      patients: patients.length,
+      latestAppointments: appointments.reverse(),
+    };
+
+    res.json({ success: true, dashData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// const deleteAppointment = async (req, res) => {
+//   try {
+//     const { appointmentId } = req.body; // Assuming the appointment ID is passed in the request body
+//     const userId = req.userId; // Extract the user ID from the validated JWT token
+//     const appointment = await appointmentModel.findOneAndDelete({
+//       _id: appointmentId,
+//       userId: userId,
+//     });
+
+//     if (!appointment) {
+//       return res.status(400).send({
+//         success: false,
+//         message: "Appointment not found ",
+//       });
+//     }
+//     const updatedAppointments = await appointmentModel.find({ userId });
+
+//     // Wait for all reformatted appointments to complete
+//     const updatedAppointmentsForUI = await Promise.all(
+//       updatedAppointments.map((appointment) =>
+//         reformatedAppointment(appointment)
+//       )
+//     );
+
+//     return res.status(200).send({
+//       success: true,
+//       message: "Appointment deleted successfully",
+//       updatedAppointments: updatedAppointmentsForUI,
+//     });
+//   } catch (error) {
+//     return res
+//       .status(500)
+//       .send({ success: false, message: "Internal server error" });
+//   }
+// };
+
+export {
+  changeAvailablity,
+  doctorList,
+  loginDoctor,
+  appointmentsDoctor,
+  appointmentCancel,
+  appointmentComplete,
+  doctorDashboard,
+  // deleteAppointment,
+};
